aa6fc9f48e8a42a9afa8f96e5ddea7cb732caeae
Laurence Roberts <lsjroberts@gmail.com>
2013-05-22 03:02:18 +0100

    Fixed chromasome fusion and fission

--- life/organism/Chromasome.py
+++ life/organism/Chromasome.py
@@ -21,7 +21,7 @@ class Chromosome( ):
 
     def meiosis( self ):
         arm = []
-        for genePair in genes:
+        for genePair in self.genes:
             gene = choice( genePair )
             arm.append( gene.copy() )
         return arm
@@ -34,32 +34,22 @@ class Chromosome( ):
     # @param Chromosome chromosome
     # @return Chromosome
     def fusion( self, chromosome ):
-        newTraits = []
-
-        for trait in self.traits:
-            trait.mutate( )
-            newTraits.append( trait )
-
-        for trait in gene.traits:
-            trait.mutate( )
-            newTraits.append( trait )
-
-        return Gene( newTraits )
+        return Chromasome( self.genes + chromosome.genes )
 
     # Create fission of the chromosome into 2 chromosomes
     # @param self
     # @return tuple Chromosome, Chromosome
     def fission( self ):
-        oldTraits = []
-        newTraits = []
-        lenTraits = len(self.traits)
+        genesOne = []
+        genesTwo = []
+        lenGenes = len(self.genes)
 
-        for i, trait in enumerate(self.traits):
-            trait.mutate( )
+        for i, gene in enumerate(self.genes):
             r = randint(0,1)
-            if r == 0 or (i == lentraits and len(newTraits) == 0):
-                newTraits.append( trait )
+            # Ensure genesTwo has at least one gene by checking it's length on the last loop
+            if r == 0 or (i == lenGenes and len(genesTwo) == 0):
+                genesTwo.append( trait )
             else:
-                oldTraits.append( trait )
+                genesOne.append( trait )
 
-        return Gene( oldTraits), Gene( newTraits )
+        return Chromasome( genesOne ), Chromasome( genesTwo )
